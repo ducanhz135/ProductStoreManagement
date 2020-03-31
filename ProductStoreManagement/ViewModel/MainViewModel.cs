b4ac1b1f852fde0059f58fe2f9052cb51d57d9b2
duc anh <ducanhz135@gmail.com>
2020-03-31 22:22:33 +0700

    commit login

--- ProductStoreManagement/ViewModel/MainViewModel.cs
+++ ProductStoreManagement/ViewModel/MainViewModel.cs
@@ -4,15 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ProductStoreManagement.ViewModel
 {
-    public class MainViewModel: BaseViewModel
+    public class MainViewModel : BaseViewModel
     {
+        public bool Isloaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+
         public MainViewModel()
         {
-            LoginWindow loginWindow = new LoginWindow();
-            loginWindow.ShowDialog();
-        } 
+            LoadedWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                Isloaded = true;
+                if (p == null)
+                    return;
+                p.Hide();
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+
+                if (loginWindow.DataContext == null)
+                    return;
+                var loginVM = loginWindow.DataContext as LoginViewModel;
+
+                if (loginVM.IsLogin)
+                {
+                    p.Show();
+                }
+                else
+                {
+                    p.Close();
+                }
+            });
+
+
+
+            //LoginWindow loginWindow = new LoginWindow();
+            //loginWindow.ShowDialog();
+        }
     }
 }
